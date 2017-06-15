1a10ef4d8c114c9309a9bcbb9306d82b8067b7c4
mariqn <marian_bochev@hotmail.com>
2017-06-15 18:16:37 +0300

    Homework 1

---
+++ Homework 1/task-4.js
@@ -0,0 +1,12 @@
+//input
+var userInput1 = Math.round(Math.random() * 99) + 1;
+var userInput2 = Math.round(Math.random() * 99) + 1;;
+
+//output
+if (userInput1 > userInput2) {
+    console.log(userInput1);
+    console.log(userInput2);
+} else {
+    console.log(userInput2);
+    console.log(userInput1);
+}   
